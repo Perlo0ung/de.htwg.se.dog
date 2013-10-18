8f2ca21ee5adfc85c92c255cb3256564ca07efb2
Michael <Michael@Perlo0ung>
2013-10-18 20:05:13 +0200

    Cardstack, added dealCard and Testfunction

--- de.htwg.se.dog/src/de/htwg/se/dog/models/CardStack.java
+++ de.htwg.se.dog/src/de/htwg/se/dog/models/CardStack.java
@@ -1,41 +1,45 @@
 package de.htwg.se.dog.models;
 
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.Random;
 import java.util.Stack;
 
 public class CardStack {
 
-	public static Stack<Card> cardstack;
+	private Stack<Card> cardstack = null;
+	private final int CARDS = 4;
+	private final int JOKER = 14;
+	private Random gen;
 	
-	public CardStack(int size){
+	public CardStack(int size) {
 		cardstack = new Stack<Card>();
 		generateStack(size);
-		shuffleStack();
-	}
-	
-	public static void generateStack(int size){
-		for(int i = 0; i <= 3 ; i++){
-			for(int j = 0; j <= size; j++){
-				cardstack.push(new Card(j+1));
-			}
-		}
-		cardstack.push(new Card(14));
-		cardstack.push(new Card(14));
-		cardstack.push(new Card(14));
-		for (Iterator iterator = cardstack.iterator(); iterator.hasNext();) {
-			Card type = (Card) iterator.next();
-			System.out.println(type.getValue()+" "+type.getCardName(type.getValue()));
-			
-		}
-		
-		
-		
-	}
-	public void shuffleStack(){
-		Collections.shuffle(cardstack);
 	}
 
+	public void generateStack(int size) {
+		for (int i = 0; i <= (CARDS-1); i++) {
+			for (int j = 0; j <= size; j++) {
+				cardstack.push(new Card(j + 1));
+			}
+		}
+		cardstack.push(new Card(JOKER));
+		cardstack.push(new Card(JOKER));
+		cardstack.push(new Card(JOKER));
+		/*
+		 * for (Iterator<Card> iterator = cardstack.iterator();
+		 * iterator.hasNext();) { Card type = (Card) iterator.next();
+		 * System.out.println(type.getValue() + " " +
+		 * type.getCardName(type.getValue())); }
+		 */
+	}
+
+	public Stack<Card> getStack() {
+		return this.cardstack;
+	}
+
+	public Card dealCard(int range) {
+		gen = new Random();
+		int index = gen.nextInt(range);
+		System.out.println(index);
+		return cardstack.remove(index);
+	}
 }
-	
--- de.htwg.se.dog/test/de/htwg/se/dog/models/CardStackTest.java
+++ de.htwg.se.dog/test/de/htwg/se/dog/models/CardStackTest.java
@@ -2,39 +2,42 @@ package de.htwg.se.dog.models;
 
 import static org.junit.Assert.*;
 
-import java.util.Collections;
-import java.util.Random;
+import java.util.Iterator;
 import java.util.Stack;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class CardStackTest {
-	
+
 	Card wrong;
-	CardStack cardStack;
+	CardStack cardStack, cardStackSorted;
 	Stack<Card> stack;
+	final int CARDS = 12;
+	final int RANGE = 1;
 	int size;
+
 	@Before
-	public void setUp() throws Exception{
-		new CardStack(12);
-		stack = CardStack.cardstack;
+	public void setUp() throws Exception {
+		cardStack = new CardStack(CARDS);
+		stack = cardStack.getStack();
 		size = stack.size();
 	}
-	
-	
+
 	@Test
 	public void testgenerateStack() {
-		//System.out.println(cardStack.size());
-		for(int i = 0; i <=51; i++){
-		int tmp = stack.elementAt(i).getValue();
-		System.out.println("I: " + i + ", Karte: " + tmp);	
-		assertEquals(i % 13 + 1, tmp);
+		for (int i = 0; i <= (4 * (CARDS + 1) - 1); i++) {
+			int tmp = stack.elementAt(i).getValue();
+			System.out.println("I: " + i + ", Karte: " + tmp);
+			assertEquals(i % (CARDS + 1) + 1, tmp);
 		}
-		
 		assertEquals(14, stack.pop().getValue());
 		assertEquals(14, stack.pop().getValue());
 		assertEquals(14, stack.pop().getValue());
 	}
 
+	@Test
+	public void testdealCard() {
+		assertEquals(cardStack.dealCard(RANGE).getValue(),1);
+	}
 }
