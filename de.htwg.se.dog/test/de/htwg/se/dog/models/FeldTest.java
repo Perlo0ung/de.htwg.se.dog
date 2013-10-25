b28edc86992019b208c7d6cb808754ff67f60658
miofferg <miofferg@3DLABOR6.in.fhkn.de>
2013-10-25 13:34:06 +0200

    Added field class

--- de.htwg.se.dog/test/de/htwg/se/dog/models/FeldTest.java
+++ de.htwg.se.dog/test/de/htwg/se/dog/models/FeldTest.java
@@ -1,40 +1,43 @@
 package de.htwg.se.dog.models;
 
 import static org.junit.Assert.*;
+import de.htwg.se.dog.models.Field;
+import de.htwg.se.dog.models.Figure;
 
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class FeldTest {	
 	private final int NOOWNER = 0;
 	private final int OWNER = 1;
 	private final int FIGNUM = 1;
-	public Feld feld1;
-	public Feld feld2;	
+	public Field feld1;
+	public Field feld2;	
 	Figure f = new Figure(OWNER,FIGNUM);
 	
-	@Test
+	@Before
 	public void setUp() {
-		feld1 = new Feld(NOOWNER);
-		feld2 = new Feld(OWNER);
+		feld1 = new Field(NOOWNER);
+		feld2 = new Field(OWNER);
 		feld2.setFigure(f);
 		feld2.setBlocked(true);
+		feld2.getBlocked();
 	}
 	@Test
 	public void testSetFigure() {
-	//	assertEquals(f, feld2.getFigure());
+		assertEquals(f, feld2.getFigure());
 	}
 	@Test
 	public void testGetFigure() {
-	//	assertEquals(f, feld2.getFigure());
+		assertEquals(f, feld2.getFigure());
 	}
 	@Test
 	public void testGetOwner(){
-		//assertEquals(OWNER, feld2.getOwner());
+		assertEquals(OWNER, feld2.getOwner());
 	}
 	@Test
 	public void setBlocked(){
-		assertEquals(true, feld2.getBlocked());
+		assertEquals(feld2.getBlocked(),true);
 	}
 	@Test
 	public void testIsHouse(){
