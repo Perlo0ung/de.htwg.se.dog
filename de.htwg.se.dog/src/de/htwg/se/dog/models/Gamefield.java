695769241e4f74d3ba0f66eeeb3a4a5e59b48ed7
chwaesch <chwaesch@htwg-konstanz.de>
2013-10-25 14:08:23 +0200

    updated Gamefield

--- de.htwg.se.dog/src/de/htwg/se/dog/models/Gamefield.java
+++ de.htwg.se.dog/src/de/htwg/se/dog/models/Gamefield.java
@@ -4,12 +4,21 @@ package de.htwg.se.dog.models;
 public class Gamefield {
 
 	Field[] field;
-	int test;
-	public Gamefield(int playercount){
+	private final int fieldsTilHouse;
+	private final int playerCount;
+	private final int houseCount;
+	
+	public Gamefield(int fieldsTilHouse, int playerCount, int houseCount){
+		this.fieldsTilHouse = fieldsTilHouse;
+		this.houseCount = houseCount;
+		this.playerCount = playerCount;
 		
-		
+		field = new Field[(fieldsTilHouse+houseCount)*playerCount];
+		generateGamefield();
 	}
 
-	
+	private void generateGamefield(){
+		
+	}
 	
 }
