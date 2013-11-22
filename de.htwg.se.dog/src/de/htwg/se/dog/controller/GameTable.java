01b6d6b0bf78bd036a92fa9c882b8394a1cb2b81
Michael <Michael@Michael-Yoga>
2013-11-22 14:00:05 +0100

    changed turn gametable

--- de.htwg.se.dog/src/de/htwg/se/dog/controller/GameTable.java
+++ de.htwg.se.dog/src/de/htwg/se/dog/controller/GameTable.java
@@ -2,7 +2,13 @@ package de.htwg.se.dog.controller;
 
 
 
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.List;
+
+import de.htwg.se.dog.models.Card;
 import de.htwg.se.dog.models.GameField;
+import de.htwg.se.dog.models.Player;
 
 public class GameTable {
 
@@ -10,7 +16,9 @@ public class GameTable {
     private static final int HOUSECOUNT = 4;
 
     GameField game;
-    Turn turn;
+    final LinkedList<Player> players;
+    ArrayDeque<Player> turnPlayer;
+    CardDealer dealer;
     
     /**
      * Consturctor to generates a new gametable
@@ -19,8 +27,57 @@ public class GameTable {
      */
     public GameTable(int playerCount, int figCount) {
         game = new GameField(FIELDSTILLHOUSE, playerCount, HOUSECOUNT);
-        turn = new Turn(playerCount, figCount);
-
+        players = new LinkedList<Player>();
+        dealer = new CardDealer(playerCount);
+        addPlayers(playerCount, figCount);
     }
-  
+    /**
+     * Add all Players to playerlist
+     * @param playerCount
+     * @param figCount
+     */
+    private void addPlayers(int playerCount, int figCount) {
+        for (int i = 1; i <= playerCount; i++) {
+            players.add(new Player(playerCount, figCount));
+        }
+    }
+    /**
+     * Returns reference to gamefield 
+     * @return
+     */
+    public GameField getGameField() {
+        return this.game;
+    }
+    
+    public void newRound() {
+        for (Player p : players) {
+            dealer.dealCards(p);
+        }
+        dealer.newRound();
+        turnPlayer = new ArrayDeque<Player>(players);
+    }
+    /**
+     * Returns the player that can playnow
+     * @return
+     */
+    public Player getCurrentPlayer() {
+        return turnPlayer.pollFirst();
+    }
+    /**
+     * TODO
+     * @param p
+     * @return
+     */
+    public boolean canPlay(Player p) {
+        boolean returnval = false;
+        List<Card> cards = p.getCardList();
+        if (cards.isEmpty()) {
+            return returnval;
+        }
+        for (Card c: cards) {
+            for (Integer field : p.getFigureRegister()){
+                Movement.validMove(
+            }
+        }
+     }
 }
--- de.htwg.se.dog/src/de/htwg/se/dog/controller/Turn.java
+++ de.htwg.se.dog/src/de/htwg/se/dog/controller/Turn.java
@@ -1,74 +1,31 @@
 package de.htwg.se.dog.controller;
 
-import java.util.ArrayDeque;
-import java.util.LinkedList;
-import java.util.List;
 
-import de.htwg.se.dog.models.Card;
 import de.htwg.se.dog.models.Player;
 
 public class Turn {
-    final LinkedList<Player> players;
-    ArrayDeque<Player> turn;
-    CardDealer dealer;
-    Player currentPlayer;
-    
+    GameTable table;
+    Player currentPlayer;    
     /**
      * Standard Konstruktor
      * @param playerCount
      * @param figCount
      */
     public Turn(int playerCount, int figCount) {
-        players = new LinkedList<Player>();
-        dealer = new CardDealer(playerCount);
-        addPlayers(playerCount, figCount);
+        table = new GameTable(playerCount, figCount);
     }
-    
-    /**
-     * Add all Players to playerlist
-     * @param playerCount
-     * @param figCount
-     */
-    private void addPlayers(int playerCount, int figCount) {
-        for (int i = 1; i <= playerCount; i++) {
-            players.add(new Player(playerCount, figCount));
-        }
-    }
-    
+
     /**
      * Deal new cards;
      */
     public void newRound() {
-        for (Player p : players) {
-            dealer.dealCards(p);
-        }
-        dealer.newRound();
-        turn = new ArrayDeque<Player>(players);
+        table.newRound();
     }
     /**
      *     
      * @return the current Player
      */
     public Player getCurrentPlayer() {
-        currentPlayer = turn.pollFirst();
-        return currentPlayer;
+        return table.getCurrentPlayer();
     }
-    /**
-     * TODO
-     * @param p
-     * @return
-     */
-    public boolean canPlay(Player p) {
-        boolean returnval = false;
-        List<Card> cards = p.getCardList();
-        if (cards.isEmpty()) {
-            return returnval;
-        }
-        for (Card c: cards) {
-            for (Integer field : p.getFigureRegister()){
-                Movement.validMove();
-            }
-        }
-    }
-    
 }
