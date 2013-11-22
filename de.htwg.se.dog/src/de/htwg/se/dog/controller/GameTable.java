b37ae9458f838a740db3cc8674aab7ad4eb6ba51
Michael <Michael@Perlo0ung>
2013-11-22 20:34:14 +0100

    gametable+test

--- de.htwg.se.dog/src/de/htwg/se/dog/controller/GameTable.java
+++ de.htwg.se.dog/src/de/htwg/se/dog/controller/GameTable.java
@@ -1,6 +1,7 @@
 package de.htwg.se.dog.controller;
 
 import java.util.ArrayDeque;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,86 +11,102 @@ import de.htwg.se.dog.models.Player;
 
 public class GameTable {
 
-    private static final int FIELDSTILLHOUSE = 16;
-    private static final int HOUSECOUNT = 4;
+	private static final int FIELDSTILLHOUSE = 16;
+	private static final int HOUSECOUNT = 4;
 
-    GameField game;
-    final LinkedList<Player> players;
-    ArrayDeque<Player> turnPlayer;
-    CardDealer dealer;
+	protected GameField game;
+	protected final LinkedList<Player> players;
+	protected ArrayDeque<Player> turnPlayer;
+	protected CardDealer dealer;
 
-    /**
-     * Consturctor to generates a new gametable
-     * 
-     * @param playerCount
-     *            : int number of players
-     * @param figCount
-     *            : int number of figures per player
-     */
-    public GameTable(int playerCount, int figCount) {
-        game = new GameField(FIELDSTILLHOUSE, playerCount, HOUSECOUNT);
-        players = new LinkedList<Player>();
-        dealer = new CardDealer(playerCount);
-        addPlayers(playerCount, figCount);
-    }
+	/**
+	 * Constructor to generate a new gametable
+	 * 
+	 * @param playerCount
+	 *            number of players
+	 * @param figCount
+	 *            number of figures per player
+	 */
+	public GameTable(int playerCount, int figCount) {
+		game = new GameField(FIELDSTILLHOUSE, playerCount, HOUSECOUNT);
+		players = new LinkedList<Player>();
+		dealer = new CardDealer(playerCount);
+		addPlayers(playerCount, figCount);
+		dealCards();
+	}
 
-    /**
-     * Add all Players to playerlist
-     * 
-     * @param playerCount
-     * @param figCount
-     */
-    private void addPlayers(int playerCount, int figCount) {
-        for (int i = 1; i <= playerCount; i++) {
-            players.add(new Player(playerCount, figCount));
-        }
-    }
-    
+	/**
+	 * Add all Players to playerlist
+	 * 
+	 * @param playerCount
+	 * @param figCount
+	 */
+	private void addPlayers(int playerCount, int figCount) {
+		for (int i = 1; i <= playerCount; i++) {
+			players.add(new Player(playerCount, figCount));
+		}
+	}
 
-    public void newRound() {
-        for (Player p : players) {
-            dealer.dealCards(p);
-        }
-        dealer.newRound();
-        turnPlayer = new ArrayDeque<Player>(players);
-    }
+	/**
+	 * Refills the Playerqueue and deals cards to every player
+	 */
+	protected void dealCards() {
+		turnPlayer = new ArrayDeque<Player>(players);
+		for (Player p : turnPlayer) {
+			dealer.dealCards(p);
+		}
+	}
 
-    /**
-     * Returns the player that can playnow
-     * 
-     * @return
-     */
-    public Player getCurrentPlayer() {
-        return turnPlayer.pollFirst();
-    }
-    
-    
-    /**
-     * Returns true if the Player has a card that can be played
-     * @param p the Player that wants to play
-     * @return true if he can play, otherwise false
-     */
-    public boolean canPlay(Player p) {
-        return !possibleCards(p).isEmpty();
-    }
-    /**
-     * Returns a list containing the cards that can be played by Player p
-     * @param p the player that wants to play
-     * @return a list containing the cards that can be played
-     */
-    public List<Card> possibleCards(Player p) {
-        boolean returnval = false;
-        List<Card> cards = new LinkedList<Card>(p.getCardList());
-        for (Card c : cards) {
-            for (Integer field : p.getFigureRegister()) {
-                if (Movement.validMove(game, c.getValue(), field)) {
-                    returnval = true;
-                }
-            }
-            if (!returnval) {
-                cards.remove(c);
-            }
-        }
-        return cards;
-    }
+	/**
+	 * Starts a new round
+	 */
+	public void newRound() {
+		dealCards();
+		dealer.newRound();
+	}
+
+	/**
+	 * Returns the player that can play now
+	 * 
+	 * @return
+	 */
+	public Player getCurrentPlayer() {
+		return turnPlayer.pollFirst();
+	}
+
+	/**
+	 * Returns true if the Player has a card that can be played
+	 * 
+	 * @param p
+	 *            the Player that wants to play
+	 * @return true if he can play, otherwise false
+	 */
+	public boolean canPlay(Player p) {
+		return !possibleCards(p).isEmpty();
+	}
+
+	/**
+	 * Returns a list containing the cards that can be played by Player p
+	 * 
+	 * @param p
+	 *            the player that wants to play
+	 * @return a list containing the cards that can be played
+	 */
+	public List<Card> possibleCards(Player p) {
+		boolean returnval = false;
+		List<Card> cards = new LinkedList<Card>(p.getCardList());
+		Iterator<Card> it = cards.iterator();
+		while (it.hasNext()) {
+			Card c = it.next();
+			for (Integer field : p.getFigureRegister()) {
+				if (Movement.validMove(game, c.getValue(), field)) {
+					returnval = true;
+				}
+			}
+			if (!returnval) {
+				it.remove();
+			}
+		}
+		return cards;
+	}
 }
--- de.htwg.se.dog/test/de/htwg/se/dog/controller/GameTableTest.java
+++ de.htwg.se.dog/test/de/htwg/se/dog/controller/GameTableTest.java
@@ -3,23 +3,29 @@ package de.htwg.se.dog.controller;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.htwg.se.dog.models.Field;
+import de.htwg.se.dog.models.Figure;
 import de.htwg.se.dog.models.Player;
 import static org.junit.Assert.*;
 
 public class GameTableTest {
     GameTable table; 
     Player first;
+    Field[] array;
     @Before
     public void setUp() {
         table = new GameTable(2, 1);
         table.newRound();
+        array = table.game.getGamefield();
+        first = table.getCurrentPlayer();
     }
     
     @Test
     public void testCurrentPlayer() {
-        first = table.getCurrentPlayer();
+        Player second = table.getCurrentPlayer();
         table.newRound();
-        assertNotEquals(first, table.getCurrentPlayer());
+        assertEquals(first, table.getCurrentPlayer());
+        assertEquals(second, table.getCurrentPlayer());
     }
     @Test
     public void testNewRound() {
@@ -28,6 +34,12 @@ public class GameTableTest {
     
     @Test
     public void testcanPlay() {
+        assertFalse(table.canPlay(first));
+        table.dealCards();
+        first = table.getCurrentPlayer();
+        Figure fig = first.removeFigure();
+        array[20].putFigure(fig);
+        first.updateFigurePos(fig.getFignr(), 20);
         assertTrue(table.canPlay(first));
     }
     
