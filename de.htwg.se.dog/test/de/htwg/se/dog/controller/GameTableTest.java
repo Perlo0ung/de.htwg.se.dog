0c15b383edb2b6ee8336699aa56980a45b900874
Michael <Michael@Michael-Yoga>
2013-11-29 13:50:15 +0100

    gametabletest

--- de.htwg.se.dog/test/de/htwg/se/dog/controller/GameTableTest.java
+++ de.htwg.se.dog/test/de/htwg/se/dog/controller/GameTableTest.java
@@ -1,17 +1,21 @@
 package de.htwg.se.dog.controller;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import de.htwg.se.dog.models.Field;
 import de.htwg.se.dog.models.Figure;
 import de.htwg.se.dog.models.Player;
-import static org.junit.Assert.*;
 
 public class GameTableTest {
-    GameTable table; 
+    GameTable table;
     Player first;
     Field[] array;
+
     @Before
     public void setUp() {
         table = new GameTable(2, 1);
@@ -19,7 +23,7 @@ public class GameTableTest {
         array = table.getGameField().getGamefield();
         first = table.getCurrentPlayer();
     }
-    
+
     @Test
     public void testCurrentPlayer() {
         Player second = table.getCurrentPlayer();
@@ -27,16 +31,17 @@ public class GameTableTest {
         assertEquals(first, table.getCurrentPlayer());
         assertEquals(second, table.getCurrentPlayer());
     }
+
     @Test
     public void testNewRound() {
         table.newRound();
     }
-    
+
     @Test
     public void testPlayerQueueIsEmpty() {
-    	assertFalse(table.playerQueueIsEmpty());
+        assertFalse(table.playerQueueIsEmpty());
     }
-    
+
     @Test
     public void testcanPlay() {
         assertFalse(table.canPlay(first));
@@ -47,5 +52,5 @@ public class GameTableTest {
         first.updateFigurePos(fig.getFignr(), 20);
         assertTrue(table.canPlay(first));
     }
-    
+
 }
