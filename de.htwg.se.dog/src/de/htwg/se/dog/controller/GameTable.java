e7c79fa284f46c0212a4fcdb6c82b995e7d8462c
Michael <Michael@Michael-Yoga>
2013-11-29 22:10:17 +0100

    gametable

--- de.htwg.se.dog/src/de/htwg/se/dog/controller/GameTable.java
+++ de.htwg.se.dog/src/de/htwg/se/dog/controller/GameTable.java
@@ -11,120 +11,124 @@ import de.htwg.se.dog.models.Player;
 
 public class GameTable {
 
-	private static final int FIELDSTILLHOUSE = 16;
-	private static final int HOUSECOUNT = 4;
+    private static final int FIELDSTILLHOUSE = 16;
+    private static final int HOUSECOUNT = 4;
 
-	private GameField game;
-	private List<Player> players;
-	private Queue<Player> turnPlayer;
-	private CardDealer dealer;
+    private GameField game;
+    private List<Player> players;
+    private Queue<Player> turnPlayer;
+    private CardDealer dealer;
+    private Movement movement;
 
-	/**
-	 * Constructor to generate a new gametable
-	 * 
-	 * @param playerCount
-	 *            number of players
-	 * @param figCount
-	 *            number of figures per player
-	 */
-	public GameTable(int playerCount, int figCount) {
-		game = new GameField(FIELDSTILLHOUSE, playerCount, HOUSECOUNT);
-		players = new LinkedList<Player>();
-		dealer = new CardDealer(playerCount);
-		addPlayers(playerCount, figCount);
-	}
+    /**
+     * Constructor to generate a new gametable
+     * 
+     * @param playerCount
+     *            number of players
+     * @param figCount
+     *            number of figures per player
+     */
+    public GameTable(int playerCount, int figCount) {
+        game = new GameField(FIELDSTILLHOUSE, playerCount, HOUSECOUNT);
+        players = new LinkedList<Player>();
+        dealer = new CardDealer(playerCount);
+        addPlayers(playerCount, figCount);
+        movement = new Movement();
+    }
 
-	/**
-	 * Returns the Gamefield currently playing on
-	 * 
-	 * @return
-	 */
-	public GameField getGameField() {
-		return this.game;
-	}
+    /**
+     * Returns the Gamefield currently playing on
+     * 
+     * @return
+     */
+    public GameField getGameField() {
+        return this.game;
+    }
 
-	/**
-	 * Returns weather the playerqueue is empty or not
-	 * 
-	 * @return true if empty otherwise false
-	 */
-	public boolean playerQueueIsEmpty() {
-		return turnPlayer.isEmpty();
-	}
+    /**
+     * Returns weather the playerqueue is empty or not
+     * 
+     * @return true if empty otherwise false
+     */
+    public boolean playerQueueIsEmpty() {
+        return turnPlayer.isEmpty();
+    }
 
-	/**
-	 * Add all Players to playerlist
-	 * 
-	 * @param playerCount
-	 * @param figCount
-	 */
-	private void addPlayers(int playerCount, int figCount) {
-		for (int i = 1; i <= playerCount; i++) {
-			players.add(new Player(playerCount, figCount));
-		}
-	}
+    /**
+     * Add all Players to playerlist
+     * 
+     * @param playerCount
+     * @param figCount
+     */
+    private void addPlayers(int playerCount, int figCount) {
+        for (int i = 1; i <= playerCount; i++) {
+            players.add(new Player(playerCount, figCount));
+        }
+    }
 
-	/**
-	 * Refills the Playerqueue and deals cards to every player
-	 */
-	public void dealCards() {
-		turnPlayer = new LinkedList<Player>();
-		for (Player p : players) {
-			dealer.dealCards(p);
-			turnPlayer.add(p);
-		}
-	}
+    /**
+     * Refills the Playerqueue and deals cards to every player
+     */
+    public void dealCards() {
+        turnPlayer = new LinkedList<Player>();
+        for (Player p : players) {
+            dealer.dealCards(p);
+            turnPlayer.add(p);
+        }
+    }
 
-	/**
-	 * Starts a new round
-	 */
-	public void newRound() {
-		dealCards();
-		dealer.newRound();
-	}
+    /**
+     * Starts a new round
+     */
+    public void newRound() {
+        dealCards();
+        dealer.newRound();
+    }
 
-	/**
-	 * Returns the player that can play now
-	 * 
-	 * @return the player that is allowed to play
-	 */
-	public Player getCurrentPlayer() {
-		return turnPlayer.poll();
-	}
+    /**
+     * Returns the player that can play now
+     * 
+     * @return the player that is allowed to play
+     */
+    public Player getCurrentPlayer() {
+        return turnPlayer.poll();
+    }
 
-	/**
-	 * Returns true if the Player has a card that can be played
-	 * 
-	 * @param p
-	 *            the Player that wants to play
-	 * @return true if he can play, otherwise false
-	 */
-	public boolean canPlay(Player p) {
-		return !possibleCards(p).isEmpty();
-	}
+    /**
+     * Returns true if the Player has a card that can be played
+     * 
+     * @param p
+     *            the Player that wants to play
+     * @return true if he can play, otherwise false
+     */
+    public boolean canPlay(Player p) {
+        return !possibleCards(p).isEmpty();
+    }
 
-	/**
-	 * Returns a list containing the cards that can be played by Player p
-	 * 
-	 * @param p
-	 *            the player that wants to play
-	 * @return a list containing the cards that can be played
-	 */
-	public List<Card> possibleCards(Player p) {
-		boolean returnval = false;
-		List<Card> cards = new LinkedList<Card>(p.getCardList());
-		Iterator<Card> it = cards.iterator();
-		while (it.hasNext()) {
-			Card c = it.next();
-			for (Integer field : p.getFigureRegister()) {
-				if (Movement.validMove(game, c.getValue(), field)) {
-					returnval = true;
-				}
-			}
-			if (!returnval) {
-				it.remove();
-			}
-		}
-		return cards;
-	}
+    /**
+     * Returns a list containing the cards that can be played by Player p
+     * 
+     * @param p
+     *            the player that wants to play
+     * @return a list containing the cards that can be played
+     */
+    public List<Card> possibleCards(Player p) {
+        boolean returnval = false;
+
+        List<Card> cards = new LinkedList<Card>(p.getCardList());
+        Iterator<Card> it = cards.iterator();
+        while (it.hasNext()) {
+            Card c = it.next();
+            for (Integer field : p.getFigureRegister()) {
+                movement.setMoveStrategie(c);
+                if (movement.validMove(game, c.getValue(), field)) {
+                    returnval = true;
+                }
+            }
+            if (!returnval) {
+                it.remove();
+            }
+        }
+        return cards;
+    }
 }
