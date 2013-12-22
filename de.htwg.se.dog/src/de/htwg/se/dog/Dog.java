12e11e0293b238abdea7ee8ccc964fb585f10dbf
Michael <Michael@Perlo0ung>
2013-12-22 10:59:30 +0100

    gui stuff

--- de.htwg.se.dog/src/de/htwg/se/dog/Dog.java
+++ de.htwg.se.dog/src/de/htwg/se/dog/Dog.java
@@ -6,6 +6,7 @@ import com.google.inject.Inject;
 
 import de.htwg.se.dog.controller.GameTableInterface;
 import de.htwg.se.dog.controller.impl.GameTable;
+import de.htwg.se.dog.tui.GuiTest;
 import de.htwg.se.dog.tui.TextUserInterface;
 
 public class Dog {
@@ -13,13 +14,13 @@ public class Dog {
     private static GameTableInterface controller;
     private static Dog instance;
     private static TextUserInterface tui;
-
+    private static GuiTest gui;
     @Inject
     public Dog() {
         controller = new GameTable(4);
         controller.newRound();
         tui = new TextUserInterface(controller);
-
+        gui = new GuiTest(controller);
     }
 
     public static void main(String[] args) {
