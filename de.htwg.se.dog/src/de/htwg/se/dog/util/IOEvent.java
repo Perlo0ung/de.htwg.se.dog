f8ab8c4f329c0d309b924217cf22711b748f7d0d
Michael <Michael@Perlo0ung>
2013-12-27 19:35:34 +0100

    updated IOEventIF

--- de.htwg.se.dog/src/de/htwg/se/dog/util/IOEvent.java
+++ de.htwg.se.dog/src/de/htwg/se/dog/util/IOEvent.java
@@ -1,5 +1,10 @@
 package de.htwg.se.dog.util;
 
+/**
+ * Interface for Event Objects
+ * @author Michael
+ *
+ */
 public interface IOEvent {
 
 }
--- de.htwg.se.dog/src/de/htwg/se/dog/util/IOMsgEvent.java
+++ de.htwg.se.dog/src/de/htwg/se/dog/util/IOMsgEvent.java
@@ -1,17 +1,24 @@
 package de.htwg.se.dog.util;
 /**
- * Abstract calls for ObserverEvents
+ * Sends String message to observers
  * @author Michael
  *
  */
 public class IOMsgEvent implements IOEvent{
 
 	private String message;
-
+	/**
+	 * inittialise the message that will be send
+	 * @param msg the message
+	 */
 	public IOMsgEvent(String msg) {
 		this.message = msg;
 	}
 
+	/**
+	 * get the string from message object
+	 * @return MsgEvent as String
+	 */
 	public String getMessage() {
 		return message;
 	}
